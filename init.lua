adf5105c06f644549360f623d57929f6922ba9b8
Maxim Avramenko <BombasterMax@yandex.ru>
2024-03-26 03:09:30 +0300

    Adjustment:
    1. Evince setted to be the pdf viewer.

--- init.lua
+++ init.lua
@@ -7,3 +7,4 @@ require('lualine').setup()
 vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
 -- for gui clients
 vim.opt.guifont = { "JetBrains Mono", ":h18" }
+vim.g.typst_pdf_viewer = "evince"

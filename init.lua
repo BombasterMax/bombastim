ef087fe4fe3780c73e9dc57be39f431733601e69
Maxim Avramenko <BombasterMax@yandex.ru>
2024-09-26 22:18:30 +0300

    Feat: copypaste && new shortcut
    1. Copypaste with ctrl+c\v in neovide.
    2. changed yazi shortcut.

--- init.lua
+++ init.lua
@@ -6,7 +6,13 @@ vim.cmd([[colorscheme tokyodark]])
 require('lualine').setup()
 -- custom key bindings
 vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
-vim.keymap.set('n', '<C-y>', '<Cmd>Yazi<CR>')
+vim.keymap.set('n', '<C-t>', '<Cmd>Yazi<CR>')
 -- for gui clients
 vim.opt.guifont = { "JetBrains Mono", ":h18" }
 vim.g.typst_pdf_viewer = "evince"
+-- copypaste for neovide
+--
+if vim.g.neovide == true then
+  vim.keymap.set({ "n", "x" }, "<C-S-C>", '"+y', { desc = "Copy system clipboard" })
+  vim.keymap.set({ "n", "x" }, "<C-S-V>", '"+p', { desc = "Paste system clipboard" })
+end
